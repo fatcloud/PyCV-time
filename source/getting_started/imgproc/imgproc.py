e1cb7f97ea3bd407cea472f60c076726ca71841c
fatcloud <victor19871226@hotmail.com>
2015-06-23 22:04:28 +0800

    imgproc

---
+++ source/getting_started/imgproc/imgproc.py
@@ -0,0 +1,8 @@
+from webcam_gui import webcam_gui
+
+def imgproc(frame):
+    
+
+
+if __name__ == "__main__":
+    webcam_gui()
---
+++ source/getting_started/webcam/webcam_gui.py
@@ -0,0 +1,31 @@
+import cv2
+
+
+
+def webcam_gui(video_index, exp_func, *arg, **kwarg):
+
+    cap = cv2.VideoCapture(video_index)
+
+    key_code = -1
+
+    while(key_code == -1):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        # image processing functions
+        frame_out = exp_func(frame, *arg, **kwarg)
+        
+        # Load the frame into a window named as 'Press any key to exit'
+        cv2.imshow('Press any key to exit', frame_out)
+        
+        # wait for the key
+        key_code = cv2.waitKey(10)
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+    
+
+if __name__ == "__main__":
+
+    webcam_gui(0, cv2.cvtColor, cv2.COLOR_BGR2GRAY)
