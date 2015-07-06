ca68ea0e68e140b73381b7054b8a9d47178a8bb6
fatcloud <victor19871226@hotmail.com>
2015-07-06 21:09:26 +0800

    make the main program look more like a piece of real code

--- laser_drawing/laser_drawing.py
+++ laser_drawing/laser_drawing.py
@@ -1,22 +1,26 @@
-def mapping(M):
-    def f(X):
-        return M*X
-    return f
+import cv2
+import numpy as np
 
-image = cap.read()
-M = calibrate( 'screen_img', image )
+from mix_image import mix_image
+from convertPts import mapping, calibrate
+from cam import OpenCV_Cam
+
+
+cam = OpenCV_Cam()
+img = cam.read()
+M = calibrate('screen_img', img)
 find_laser = mapping(M)
-canvas = np.full(...)
+canvas = np.full((600,800,4), 255, dtype=np.uint8)
 
 while True:
-    image = cap.read()
-    x, y = get_laser( image )
+    img    = cam.read()
+    x, y   = get_laser(img)
     lx, ly = find_laser(x, y)
-    canvas = draw_point( canvas, (lx, ly) )
-    out = mix_img(image, canvas)
-    imshow(out)
+    canvas = draw_point(canvas, (lx, ly))
+    out    = mix_image(img, canvas)
+    cv2.imshow(,out)
     k = waitkey(10)
     if k == ord( 'c' ):
         calibrate again
     else if  k == ord( 'r' ):
-        canvas = np.full(...)
+        canvas = np.full((600,800,4), 255, dtype=np.uint8)
