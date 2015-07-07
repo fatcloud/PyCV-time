6c9a41b3a8191ae77a4eb8751b827237a95aa7fd
fatcloud <victor19871226@hotmail.com>
2015-07-07 23:56:42 +0800

    first draft that work

--- laser_drawing/laser_drawing.py
+++ laser_drawing/laser_drawing.py
@@ -8,10 +8,15 @@ from cam import OpenCV_Cam
 
 
 def draw_burning(canvas, location):
-    cv2.circle(canvas, (lx, ly), 2, (0, 0, 255, 255))
+    cv2.circle(canvas, (lx, ly), 5, (255, 0, 0, 255))
     return canvas
-
-
+    
+    
+def laser_location(img):
+    red_part = img[:,:,2]
+    ly, lx = np.unravel_index(red_part.argmax(), red_part.shape)
+    return lx, ly
+    
 background = cv2.imread('wood.png')
 cv2.imshow('Burn this page!', background)
 
@@ -21,26 +26,25 @@ sf.set_screen_img(background)
 cam = OpenCV_Cam()
 img = cam.read()
 sf.find_screen_img(img)
-sf.find_screen_loop(cam, True)
+sf.find_screen_loop(cam, False)
 
 bs = background.shape
 canvas = np.full((bs[1], bs[0], 4), 0, dtype=np.uint8)
 
 while True:
-    img    = cam.read()
+    img = cam.read()
     
     top_view = sf.screen_top_view(img)
     cv2.imshow('Top view', top_view)
     
-    lx, ly = cv2.minMaxLoc(top_view[2])[3]
-    print cv2.minMaxLoc(top_view[2])
+    lx, ly = laser_location(top_view)
     canvas = draw_burning(canvas, (lx, ly))
     out    = mix_image(background, canvas)
     cv2.imshow('Burn this page!', out)
     
     k = cv2.waitKey(10)
     if k == ord( 'c' ):
-        sf.find_screen_loop(cam, True)
+        sf.find_screen_loop(cam, False)
     elif k == ord( 'r' ):
         canvas = np.full((bs[1], bs[0], 4), 0, dtype=np.uint8)
     elif k == 27:
