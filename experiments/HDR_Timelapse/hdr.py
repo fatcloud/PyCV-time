226189db97de8f629234029be9ad96146aa51c5d
fatcloud <victor19871226@hotmail.com>
2015-07-24 15:53:22 +0800

    replace simple add with addweighted

--- experiments/HDR_Timelapse/hdr.py
+++ experiments/HDR_Timelapse/hdr.py
@@ -10,14 +10,14 @@ if __name__ == '__main__':
     cam.set('EXPOSURE', 0)
     dst = np.full((h, w, 3), 0, dtype=np.uint8)
     
-    exp_list = range(0, -9, -1)
+    exp_list = range(-2, -9, -1)
     ratio = 1.0 / len(exp_list)
     for e in exp_list:
         cam.set('EXPOSURE', e)
         img = None
         while img is None:
             img = cam.read()
-        dst = dst + img * ratio
+        dst = cv2.addWeighted(dst, 1.0, img, ratio,0)
         
         cv2.imshow(str(e), img)
         cv2.imshow('dst'+str(e), dst)
