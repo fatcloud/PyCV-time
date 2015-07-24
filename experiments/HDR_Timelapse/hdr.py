078d5a4720ed01d63435c0b1ee0619ff62ac9e12
fatcloud <victor19871226@hotmail.com>
2015-07-24 14:27:55 +0800

    hdr exp

---
+++ experiments/HDR_Timelapse/hdr.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+from cam import OpenCV_Cam
+
+
+if __name__ == '__main__':
+
+    cam = OpenCV_Cam()
+    w,h = cam.size
+    cam.set('EXPOSURE', 0)
+    dst = np.full((h, w, 3), 0, dtype=np.uint8)
+    
+    exp_list = range(0, -9, -1)
+    ratio = 1.0 / len(exp_list)
+    for e in exp_list:
+        cam.set('EXPOSURE', e)
+        img = None
+        while img is None:
+            img = cam.read()
+        dst = dst + img * ratio
+        
+        cv2.imshow(str(e), img)
+        cv2.imshow('dst'+str(e), dst)
+        cv2.waitKey(10)
+    
+    cv2.imshow('mix', dst)
+    
+    k = cv2.waitKey(0)
---
+++ experiments/HDR_Timelapse/readme.md
@@ -0,0 +1,11 @@
+##HDR and Time-lapse photography
+
+這個範例將展示縮時攝影以及高動態範圍攝影。
+
+執行縮時攝影範例之前，
+電腦上必須先安裝 XVID Codec，
+系統才能即時錄製壓縮影片。
+
+你可以在 [xvid.com](https://www.xvid.com/download/) 下載到 XVID codec 的 windows 及 linux 安裝檔，另外在 [這邊](http://www.xvidmovies.com/codec/) 有提供 Mac 的安裝檔。
+
+此外也可以試著去使用 [其他的 Fourcc](http://www.fourcc.org/codecs.php)
