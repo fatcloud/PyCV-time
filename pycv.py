088fbd93514b69ddff23dbc28ce46ffc93dfcacc
Frederic Go <fredericstgo@gmail.com>
2015-09-29 17:30:43 +0800

    hi

---
+++ pycv.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import sys, os
+
+# get the script file name
+try:
+    script_path = sys.argv[1]
+except IndexError:
+    print 'require one argument a4s the script to run with python 2.7 + opencv 2.4.11'
+    exit()
+
+script_name = script_path.split('/')[-1]
+
+script_folder = None
+# get the script folder path where the file stays
+if script_name == script_path:
+    script_folder = sbp.check_output('pwd')
+else:
+    script_folder = '/'.join(script_path.split('/')[0:-1])
+
+arg_mount = '-v ' + script_folder + ':/exp'
+arg_image = 'fatcloud/py27-cv24-gui-camera'
+arg_cmd   = 'python ' + '/exp/' + script_name
+
+args = [
+    'docker run -it --rm',
+    '-e DISPLAY=192.168.0.101:0',
+    '-w /exp',
+    '-v $HOME/.Xauthority:/home/developer/.Xauthority',
+    '--net=host',
+    '--privileged',
+    arg_mount,
+    arg_image,
+    arg_cmd
+]
+
+cmd = ' '.join(args)
+
+print '[py27-cv24] start running in bash:\n' + cmd
+os.system(cmd)
