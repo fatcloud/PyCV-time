40de9b1b728ae9350c8ac7cc12774638be0bde2b
fatcloud <victor19871226@hotmail.com>
2015-10-10 03:09:48 +0800

    output some message

--- pycv-utils/techtree/build-graph.py
+++ pycv-utils/techtree/build-graph.py
@@ -4,14 +4,22 @@ import importlib
 import json
 import sys
 
+
 dirname = "../../experiments/"
+
+print 'Graph builder start running... looking for algorithms in ' + dirname
 for algorithm in listdir('algorithms'):
 	#buildTree = getattr(algorithm, 'main'
 	if isdir("algorithms/" + algorithm):
 		m = importlib.import_module("algorithms." + algorithm + ".main") 
+		print 'Algorithm \"' + algorithm + '\" found, start running...',
 		b = m.main(dirname)
-		with open("graphs/" + algorithm + ".json", 'w') as f:
+		
+		jsonname = "graphs/" + algorithm + ".json"
+		print 'done. Save to', jsonname
+		with open(jsonname, 'w') as f:
 			f.write(b.graph)
 
 with open("algorithms.json", 'w') as f:
 	f.write( json.dumps([i for i in listdir('algorithms') if isdir("algorithms/" + i)] ))
+
