627ceb6adff6a2f954048b7641ac3b68d19ef019
Hsin-Tien, Chu <musicandsky@gmail.com>
2016-08-02 17:50:51 +0800

    Add support for presenter and continuing frame capture
    Click the right click of the presenter will trigger frame capture.

--- experiments/stop_motion_tool/stop_motion_tool.py
+++ experiments/stop_motion_tool/stop_motion_tool.py
@@ -1,5 +1,7 @@
 from cam import OpenCV_Cam
 import cv2
+import os.path
+
 
 cam = OpenCV_Cam(0)
 cam.size = (1920, 1080)
@@ -9,8 +11,13 @@ KEY_SPACE = ord(' ')
 prevFrame = None
 i = 0 
 
+fname="frame.png"
+if os.path.isfile(fname):
+    prevFrame = cv2.imread(fname)
+
+
 fourcc = cv2.cv.CV_FOURCC(*'XVID')
-video = cv2.VideoWriter('output.avi',fourcc, 3.0, (1920,1080), isColor =True)
+video = cv2.VideoWriter('output.avi',fourcc, 3.0, cam.size, isColor =True)
 
 
 while True:
@@ -31,8 +38,8 @@ while True:
     
     # wait for the key
     key_code = cv2.waitKey(10)
-	
-    if key_code is KEY_SPACE:
+    
+    if key_code is KEY_SPACE or key_code == 2228224:
         cv2.imwrite('frame'+str(i)+'.png', frame)
         video.write(frame)
         prevFrame = frame
