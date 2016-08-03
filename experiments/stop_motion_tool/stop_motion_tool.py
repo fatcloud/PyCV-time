ece30fdb205f2f6e77d895218452b3d5939c282d
Hsin-Tien, Chu <musicandsky@gmail.com>
2016-08-03 17:51:51 +0800

    make a new directory
    as running stop_motion_tool.py, make a new directory named by current
    date and time, and store all the frame and video inside.

--- experiments/stop_motion_tool/stop_motion_tool.py
+++ experiments/stop_motion_tool/stop_motion_tool.py
@@ -1,24 +1,33 @@
 from cam import OpenCV_Cam
 import cv2
 import os.path
-
+import time
 
 cam = OpenCV_Cam(0)
 cam.size = (1920, 1080)
 
 KEY_ESC = 27
 KEY_SPACE = ord(' ')
+PAGE_DOWN = 2228224 # This make the stop motion to be controllable by presenter. 
 prevFrame = None
 i = 0 
 
-fname="frame.png"
+
+#Make a directory on current working directory with date and time as its name
+timestr = time.strftime("%Y%m%d-%H%M%S")
+cwd = os.getcwd()
+dirName = cwd + "\\"+timestr 
+os.makedirs(dirName)
+
+
+fname= cwd + "\\frame_.png"
 if os.path.isfile(fname):
     prevFrame = cv2.imread(fname)
 
 
+#Make .avi file from collected frames
 fourcc = cv2.cv.CV_FOURCC(*'XVID')
-video = cv2.VideoWriter('output.avi',fourcc, 3.0, cam.size, isColor =True)
-
+video = cv2.VideoWriter(dirName+"\\"+'output_.avi',fourcc, 3.0, cam.size, isColor =True)
 
 while True:
     # Capture frame-by-frame
@@ -39,12 +48,13 @@ while True:
     # wait for the key
     key_code = cv2.waitKey(10)
     
-    if key_code is KEY_SPACE or key_code == 2228224:
-        cv2.imwrite('frame'+str(i)+'.png', frame)
+    if key_code is KEY_SPACE or key_code == PAGE_DOWN:
+        cv2.imwrite(dirName+"\\"+'frame'+str(i)+'_.png', frame)
         video.write(frame)
         prevFrame = frame
         i += 1
     elif key_code is KEY_ESC:
+        cv2.imwrite(cwd + '\\frame_.png', prevFrame)
         break
 cv2.destroyAllWindows()	
 cam.release()
